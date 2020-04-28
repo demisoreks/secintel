1a0a973034eb3a3ed116f5c9035f1e5248651b67
Demilade Soremekun <demiladesoremekun@gmail.com>
2020-04-29 00:30:14 +0100

    Updates

--- app/Http/Controllers/Incidents.php
+++ app/Http/Controllers/Incidents.php
@@ -13,7 +13,9 @@ use App\SecIncidentType;
 class Incidents extends Controller
 {
     public function getByState($state_id) {
-        $incidents = SecIncident::where('show', true)->where('state_id', $state_id)->where('incident_date', '<=', DB::raw('DATE(NOW()) - INTERVAL 7 DAY'))->orderBy('incident_date')->get();
+        $from = date('Y-m-d', strtotime('-6 days'));
+        $to = date('Y-m-d');
+        $incidents = SecIncident::where('show', true)->where('state_id', $state_id)->whereBetween('incident_date', [$from, $to])->orderBy('incident_date')->get();
         foreach ($incidents as $incident) {
             $incident->state = SecState::find($state_id)->name;
             $incident->type = SecIncidentType::find($incident->incident_type_id)->description;
