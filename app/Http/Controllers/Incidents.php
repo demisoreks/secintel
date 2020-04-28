9a2e1719f30faceabc29fd1b94c8e3773fa67c68
Demilade Soremekun <demiladesoremekun@gmail.com>
2020-04-28 21:12:15 +0100

    Updates

--- app/Http/Controllers/Incidents.php
+++ app/Http/Controllers/Incidents.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
+
 use App\SecIncident;
 use App\SecState;
 use App\SecIncidentType;
@@ -11,7 +13,7 @@ use App\SecIncidentType;
 class Incidents extends Controller
 {
     public function getByState($state_id) {
-        $incidents = SecIncident::where('show', true)->where('state_id', $state_id)->get();
+        $incidents = SecIncident::where('show', true)->where('state_id', $state_id)->where('incident_date', '>=', DB::raw('DATE(NOW()) - INTERVAL 7 DAY'))->orderBy('incident_date')->get();
         foreach ($incidents as $incident) {
             $incident->state = SecState::find($state_id)->name;
             $incident->type = SecIncidentType::find($incident->incident_type_id)->description;
